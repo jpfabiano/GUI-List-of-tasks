705d7d42ac67dda0ae468278b2c9809204740712
João <jpfabiano08@gmail.com>
2023-12-05 10:29:36 -0300

    git commit -m "commit final"

--- src/GuiLista.java
+++ src/GuiLista.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-
+import java.util.Random;
 public class GuiLista extends JFrame {
     private JLabel jlPendentes, jlConcluidas, jlInformativo1, jlInformativo2;
     private JCheckBox cbP1,cbP2,cbP3,cbP4 ,cbC1,cbC2,cbC3,cbC4;
@@ -12,10 +12,11 @@ public class GuiLista extends JFrame {
     private JMenuBar mbMenu;
     private JMenu jmAcoes;
     private JMenuItem miCriar, miEditar;
-    private String obterTarefa;
+    private String obterTarefa, editarTarefa, novaTarefa;
     public GuiLista (){
         inicializarComponentes();
         definirEventos();
+        mensagemMotivadora();
     }
 
     public void inicializarComponentes(){
@@ -84,6 +85,10 @@ public class GuiLista extends JFrame {
                     cbP2.setVisible(true);
                     cbP3.setVisible(true);
                     cbP4.setVisible(true);
+                    cbC1.setVisible(true);
+                    cbC2.setVisible(true);
+                    cbC3.setVisible(true);
+                    cbC4.setVisible(true);
                 } else {
                     JOptionPane.showMessageDialog(null,"Termine uma tarefa para adicionar outra!");
                 }
@@ -94,17 +99,12 @@ public class GuiLista extends JFrame {
             public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == ItemEvent.SELECTED) {
                     String aux = cbP1.getText();
-                    cbP1.setText("");
                     cbP1.setSelected(false);
 
                     cbC4.setText(cbC3.getText());
                     cbC3.setText(cbC2.getText());
                     cbC2.setText(cbC1.getText());
                     cbC1.setText(aux);
-                    cbC1.setVisible(true);
-                    cbC2.setVisible(true);
-                    cbC3.setVisible(true);
-                    cbC4.setVisible(true);
 
                     cbP1.setText(cbP2.getText());
                     cbP2.setText(cbP3.getText());
@@ -113,6 +113,137 @@ public class GuiLista extends JFrame {
                 }
             }
         });
+        cbP2.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String aux = cbP2.getText();
+                    cbP2.setSelected(false);
+
+                    cbC4.setText(cbC3.getText());
+                    cbC3.setText(cbC2.getText());
+                    cbC2.setText(cbC1.getText());
+                    cbC1.setText(aux);
+
+                    cbP2.setText(cbP3.getText());
+                    cbP3.setText(cbP4.getText());
+                    cbP4.setText("");
+                }
+            }
+        });
+        cbP3.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String aux = cbP3.getText();
+                    cbP3.setSelected(false);
+
+                    cbC4.setText(cbC3.getText());
+                    cbC3.setText(cbC2.getText());
+                    cbC2.setText(cbC1.getText());
+                    cbC1.setText(aux);
+
+                    cbP3.setText(cbP4.getText());
+                    cbP4.setText("");
+                }
+            }
+        });
+        cbP4.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String aux = cbP4.getText();
+                    cbP4.setText("");
+                    cbP4.setSelected(false);
+
+                    cbC4.setText(cbC3.getText());
+                    cbC3.setText(cbC2.getText());
+                    cbC2.setText(cbC1.getText());
+                    cbC1.setText(aux);
+                }
+            }
+        });
+        cbC1.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    cbC1.setSelected(false);
+
+                    cbC1.setText(cbC2.getText());
+                    cbC2.setText(cbC3.getText());
+                    cbC3.setText(cbC4.getText());
+                    cbC4.setText("");
+                }
+            }
+        });
+        cbC2.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    cbC2.setSelected(false);
+
+                    cbC2.setText(cbC3.getText());
+                    cbC3.setText(cbC4.getText());
+                    cbC4.setText("");
+                }
+            }
+        });
+        cbC3.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    cbC3.setSelected(false);
+
+                    cbC3.setText(cbC4.getText());
+                    cbC4.setText("");
+                }
+            }
+        });
+        cbC4.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    cbC4.setSelected(false);
+
+                    cbC4.setText("");
+                }
+            }
+        });
+        miEditar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                editarTarefa = JOptionPane.showInputDialog(null,"Qual a tarefa que você deseja editar ? " +
+                        "(escreva do jeito que ela se encontra)");
+                novaTarefa = JOptionPane.showInputDialog(null,"Qual será o novo nome dela ? ");
+                if(cbP1.getText().equals(editarTarefa)){
+                    cbP1.setText(novaTarefa);
+                } else if(cbP2.getText().equals(editarTarefa)){
+                    cbP2.setText(novaTarefa);
+                } else if(cbP3.getText().equals(editarTarefa)){
+                    cbP3.setText(novaTarefa);
+                } else if(cbP4.getText().equals(editarTarefa)){
+                    cbP4.setText(novaTarefa);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Não foi encontrada nenhuma tarefa com o nome " +
+                            editarTarefa);
+                }
+            }
+        });
+    }
+
+    public void mensagemMotivadora(){
+        String[] meuArray = {"Acredite em você mesmo. Você é mais capaz do que imagina.",
+                "As dificuldades de hoje serão os degraus para o seu sucesso amanhã." ,
+                "Cada pequeno passo te aproxima do seu grande objetivo." ,
+                "Persistência e determinação são chave para alcançar seus sonhos." ,
+                "Você é o arquiteto da sua própria felicidade. Construa-a com positividade." ,
+                "A jornada pode ser difícil, mas a recompensa valerá a pena." ,
+                "Acredite. Você é capaz de superar qualquer desafio que aparecer.",
+                "O fracasso é apenas uma oportunidade para recomeçar com mais sabedoria." ,
+                "Pequenas atitudes diárias podem levar a grandes mudanças." ,
+                "Não desista. Seja paciente e persistente, seus esforços serão recompensados."};
+        Random random = new Random();
+        int x = random.nextInt(10);
+        JOptionPane.showMessageDialog(null,""+meuArray[x]);
     }
 
     public static void main (String args[]){
